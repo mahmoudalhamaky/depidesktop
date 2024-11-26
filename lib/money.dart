799d8ad01794fae3150d3975d008f53f7084ec04
mahmoudAlhamaky <39617702+mahmoudalhamaky@users.noreply.github.com>
2024-11-26 22:37:22 +0200

    try

--- lib/money.dart
+++ lib/money.dart
@@ -41,7 +41,7 @@ class _moneyState extends State<money2> {
               width: double.infinity,
               child: Center(
                   child: Text(
-                    "Money Save",
+                    "Money    Save",
                     style:
                     TextStyle(color: Colors.white70, fontSize: widtha / 10),
                   )),
